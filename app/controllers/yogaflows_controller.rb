377cee3cfe93cb08684b5fdc0d64a30b08831df3
Raghu Betina <raghu@firstdraft.com>
2018-11-16 19:17:16 +0000

    Only yogaflow user can modify yogaflow

--- app/controllers/yogaflows_controller.rb
+++ app/controllers/yogaflows_controller.rb
@@ -1,4 +1,14 @@
 class YogaflowsController < ApplicationController
+  before_action :current_user_must_be_yogaflow_user, :only => [:edit_form, :update_row, :destroy_row]
+
+  def current_user_must_be_yogaflow_user
+    yogaflow = Yogaflow.find(params["id_to_display"] || params["prefill_with_id"] || params["id_to_modify"] || params["id_to_remove"])
+
+    unless current_user == yogaflow.user
+      redirect_to :back, :alert => "You are not authorized for that."
+    end
+  end
+
   def index
     @yogaflows = Yogaflow.all
 
